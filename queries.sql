59a596469de2ae33bddb0554b8ac28602ed2368a
kingno3l <nuelluiz@gmail.com>
2023-11-03 17:27:05 +0100

    mulitple tables queried

--- queries.sql
+++ queries.sql
@@ -14,27 +14,26 @@ SELECT * FROM animals WHERE weight_kg BETWEEN  10.4 AND 17.3;
 BEGIN;
 UPDATE animals
 SET species = 'unspecified';
-SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 BEGIN;
 UPDATE animals
 SET species = 'digimon'
 WHERE name LIKE '%mon';
 SELECT * FROM animals;
-COMMIT;
 
 BEGIN;
 UPDATE animals
 SET species = 'pokemon'
 WHERE species IS null;
-SELECT * FROM animals;
 COMMIT;
+SELECT * FROM animals;
 
 BEGIN;
 DELETE FROM animals;
-SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
@@ -52,3 +51,48 @@ SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' 
 GROUP BY species;
+
+
+SELECT name, full_name
+FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.name as animal, species.name as species
+FROM animals
+JOIN species
+ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+SELECT full_name as owner, name as animal
+FROM animals
+FULL JOIN owners
+ON animals.owner_id = owners.id;
+
+SELECT species.name as species, Count(animals.name) as counted
+FROM animals
+JOIN species
+ON animals.species_id = species.id
+GROUP BY species.name;
+
+SELECT animals.name as animals, species.name as species, full_name as owner
+FROM animals
+JOIN species
+ON animals.species_id = species.id
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE species.name = 'Digimon' AND full_name = 'Jennifer Orwell';
+
+SELECT name as animal, escape_attempts, full_name as owner
+FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE full_name = 'Dean Winchester' AND escape_attempts = 0;
+
+SELECT full_name as owner, Count(name) as count_of_animals
+FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+GROUP BY full_name
+ORDER BY count_of_animals Desc;
